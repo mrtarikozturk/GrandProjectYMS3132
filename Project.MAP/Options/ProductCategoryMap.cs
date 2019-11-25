7d82d11ff9a8373315c72c9bbfbc806ffa439f3b
esrasnck <esrasancak@hotmail.com>
2019-11-25 03:00:44 +0300

    ProductCategory Tablosu eklendi. Map katmanındaki ayarlamalar yapıldı. MyContext sınıfında düzenlemeler yapıldı. Ayrıca Bogus'dan farklı bir algoritma ile (Tarık'ın algoritması) veri çekildi. Eski algoritma pasifize edilip, region a alındı.

---
+++ Project.MAP/Options/ProductCategoryMap.cs
@@ -0,0 +1,18 @@
+﻿using Project.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public class ProductCategoryMap:BaseMap<ProductCategory>
+    {
+        public ProductCategoryMap()
+        {
+            Ignore(x => x.ID);
+            HasKey(x => new { x.ProductID, x.CategoryID });
+        }
+    }
+}
--- Project.MODEL/Entities/Product.cs
+++ Project.MODEL/Entities/Product.cs
@@ -8,6 +8,11 @@ namespace Project.MODEL.Entities
 {
    public class Product:BaseEntity
     {
+        public Product()
+        {
+            Categories = new List<ProductCategory>();
+        }
+
         public string ProductName { get; set; }
         public string ImagePath { get; set; }
         public int UnitsInStock { get; set; }
@@ -15,7 +20,8 @@ namespace Project.MODEL.Entities
 
         // relational properties
 
-        public virtual List<Category> Categories { get; set; }
+        public virtual List<ProductCategory> Categories { get; set; }
+
 
         public virtual List<OrderDetail> OrderDetails { get; set; }
 
