6e716220d31ae08da05d6a0d1cd26dac7509b212
esrasnck <esrasancak@hotmail.com>
2019-11-27 01:02:43 +0300

    Tekrar duzenlenenler:
    1) Order Tablosunda, Shipped Adress silindi. yerine Kargolama Apisindeki propertyler eklendi.
       Eklenenler: Address, City, Town, District, phone
    
    2) OrderMap' de yeni eklenen propertyler turkcelestirildi
    
    3) OrderStatus, PackingStatus, PaymentOptions isimli uc tane yeni enum olusturuldu.
       OrderStatus=> Approved= onaylandı
         Canceled= Iptal edildi
         Pending= odeme bekliyor
    
       PackingStatus=> Preparing= hazırlanıyor
           Packed= Paketlendi
    
      PaymentOptions=> Transfer= havale
           Cash= kapıda/nakit odeme
           CreditCard= KrediKartı ile ödeme
    
    4) OrderDetail tablosuna yeni propertyler eklendi
        Amount= miktar
        PaymentDate= OdemeTarihi
        OrderStatus= SiparisDurumu
        PackingStatus= paketlenme durumu
        paymentOption= odemeSecenekleri
    
    5) OrderDetailMap Tablosunda yeni eklenen propertyler turkcelestirildi.
    
    6) MyContext sınıfında, Tablo isimleri turkcelestirildi.

---
+++ Project.MODEL/Enums/PaymentOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MODEL.Enums
+{
+    public enum PaymentOption
+    {
+       Transfer = 1, Cash=2, CreditCard=3
+    }
+}
