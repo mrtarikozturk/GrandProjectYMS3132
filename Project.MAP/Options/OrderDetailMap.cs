c2a2c28de233a583df5a48500d850b5a43913b63
esrasnck <esrasancak@hotmail.com>
2019-11-27 01:36:51 +0300

    duzeltıldı

--- Project.MAP/Options/OrderDetailMap.cs
+++ Project.MAP/Options/OrderDetailMap.cs
@@ -14,7 +14,14 @@ namespace Project.MAP.Options
             Ignore(x => x.ID);
             HasKey(x => new { x.OrderID, x.ProductID });
 
+            Property(x => x.Amount).HasColumnName("Miktar");
             Property(x => x.TotalPrice).HasColumnName("ToplamUcret").HasColumnType("money");
+            Property(x => x.PaymentDate).HasColumnName("OdemeTarihi");
+            Property(x => x.OrderStatus).HasColumnName("SiparisDurumu");
+            Property(x => x.PackingStatus).HasColumnName("PaketlemeDurumu");
+            Property(x => x.PaymentOption).HasColumnName("OdemeSecenekleri");
+
+            
         }
     }
 }
